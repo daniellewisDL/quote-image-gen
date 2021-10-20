7232f1aac28eb1321044a79928e5c7537c355a89
Daniel Lewis <danlewis.com@gmail.com>
2021-10-20 18:40:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -150,8 +150,9 @@ def generate_vid_and_quote(num_words=2):
     cap = cv2.VideoCapture(video_path)
     max_target_dim = 800
 
+    st.info("whoethi")
     if cap.isOpened():
-
+        st.info("whoethi")
         fourcc = cv2.VideoWriter_fourcc('H', '2', '6', '4')
         fps = cap.get(cv2.CAP_PROP_FPS)
         source_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -248,7 +249,7 @@ def generate_vid_and_quote(num_words=2):
 
             if frame_count >= max_frames: break
             
-        out.release() 
+        out.release()
     cap.release()
 
     f = open('temp_op.mp4', 'rb')
