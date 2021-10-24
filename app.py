f0e9dc8245e86833b4cb7f1ed3b7a1ec33f067ca
Daniel Lewis <danlewis.com@gmail.com>
2021-10-24 13:46:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,16 +4,14 @@ from io import BytesIO
 import random
 import json
 import string
-from string import ascii_letters
 import textwrap
-import math
 import pandas as pd
 import streamlit as st
 pexels_api_key = st.secrets["pexels_api_key"]
 
 # standard list of stopwords to remove
 
-stopwords = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there',
+stopwords1 = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there',
              'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they',
              'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into',
              'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who',
@@ -29,7 +27,7 @@ stopwords = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there'
              'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it',
              'how', 'further', 'was', 'here', 'than', 'isnt', 'dont'}
 
-cat_list = ['vegan', 'inspirational']
+stopwords = {''}
 
 @st.cache
 def get_quote_data():
@@ -39,10 +37,15 @@ def get_quote_data():
 
 def get_quote():
     my_quote_data = get_quote_data()
+    
+    # Give a 1 in 10 chance of a vegan related quotation
+    
     if random.randint(0,9) == 0:
         sampled_quote = my_quote_data[my_quote_data['Category']=="vegan"].sample(n=1)
     else:
-        sampled_quote = my_quote_data[my_quote_data['Category']=="inspirational"].sample(n=1)
+        #sampled_quote = my_quote_data[my_quote_data['Category']=="inspirational"].sample(n=1)
+        sampled_quote = my_quote_data.sample(n=1)
+    
     return sampled_quote['Quote'].iloc[0], sampled_quote['Author'].iloc[0]
 
 def choose_random_word_from_quote(quote, num_words):
@@ -63,7 +66,7 @@ def choose_random_word_from_quote(quote, num_words):
 
 def get_img(query_term):
 
-    query = query_term
+    query = query_term + " random"
     per_page=80
     pexels_colors = ['red', 'orange', 'yellow', 'green', 'turquoise', 'blue', 'violet', 'pink', 'brown', 'black', 'gray', 'white', '']
     color_req = random.choice(pexels_colors)
@@ -87,7 +90,9 @@ def get_img(query_term):
         img = Image.open(BytesIO(img_request.content))
         return img, img_json_from_search_req['photos'][random_index]['src']['original'], img_json_from_search_req['photos'][random_index]['photographer'], img_json_from_search_req['photos'][random_index]['photographer_url']
 
-def wrap_nicely(text_to_wrap):
+def wrap_nicely(text_to_wrap, aspect_ratio):
+    # TODO - buiding aspect ratio functionality
+    
     # Lower starting point for width is longest single word, or 15 whichever is higher
     lower_width = max(15, len(max(text_to_wrap.split(" "), key=len)))
     
@@ -113,25 +118,15 @@ def wrap_nicely(text_to_wrap):
     
     return textwrap.wrap(text=text_to_wrap, width=best_width)
 
+def gen_text_wrapped_image(size, quote, author=""):
 
-def generate_image_and_quote(num_words=2):
-    
-    # Request a quote from consol.csv according to the topic_to_obtain
-    quote, author = get_quote()
-
-    # Choose two (or num_words) random words from the quote
-    query = choose_random_word_from_quote(quote, num_words)
-
-    # Obtain an image on the basis of the query
-    my_quote_image, photo_link, photo_author, photographer_url = get_img(query)
-    
     # Create new image of size as Pexels returned image, with a transparency channel of 0
-    txt = Image.new("RGBA", my_quote_image.size, (255,255,255,0))
+    text_wrapped_image = Image.new("RGBA", size, (255,255,255,0))
     
-    draw = ImageDraw.Draw(txt)
+    draw = ImageDraw.Draw(text_wrapped_image)
     text = quote
 
-    wrapped_text_list = wrap_nicely(text)
+    wrapped_text_list = wrap_nicely(text, round(size[0]/size[1],2))
     wrapped_text = "\n".join(wrapped_text_list)
     longest_line = max(wrapped_text_list, key=len)
 
@@ -144,8 +139,8 @@ def generate_image_and_quote(num_words=2):
     
     # We want the longest word including punctuation to fit on one line
     
-    width_to_fill = int(max_quote_width_ratio * my_quote_image.size[0])
-    height_to_not_overflow = int(max_quote_height_ratio * my_quote_image.size[1])
+    width_to_fill = int(max_quote_width_ratio * size[0])
+    height_to_not_overflow = int(max_quote_height_ratio * size[1])
     pixels_per_char_x = int( width_to_fill / len(longest_line))
     
     # Guess font size, then test width in pixels of longest line, then scale to fill
@@ -160,30 +155,61 @@ def generate_image_and_quote(num_words=2):
         font_rows_height_stacked = font_rows_height_stacked + font.getsize(item[0])[1]
     if font_rows_height_stacked > height_to_not_overflow:
         font_size_x = int(font_size_x * height_to_not_overflow / font_rows_height_stacked )
-    
+    font = ImageFont.truetype('arial.ttf', font_size_x)
+
+    # Generate font size for the attribution
+    max_attrib_ratio_height = 0.05
+    max_attrib_ratio_width = 0.9
+    font_size_for_attrib = 24 # Guess at 24
     font_attrib = ImageFont.truetype('arial.ttf', 24)
+    font_size_for_attrib = int(font_size_for_attrib * max_attrib_ratio_height / (font_attrib.getsize(author)[1] / size[1]))
+    font_attrib = ImageFont.truetype('arial.ttf', font_size_for_attrib)
+    if font_attrib.getsize(author)[0] > (max_attrib_ratio_width * size[0]):
+        font_size_for_attrib = int((font_size_for_attrib * max_attrib_ratio_width) / (font_attrib.getsize(author)[0] / size[0]))
+    font_attrib = ImageFont.truetype('arial.ttf', font_size_for_attrib)
 
     xclr1, xclr2, xclr3 = 0, 0, 0
     opac = 255
-    draw.text(xy=(txt.size[0]/20, txt.size[1]/20), text=wrapped_text, font=font, fill=(255-xclr1,255-xclr2,255-xclr3,opac))
-    draw.text(xy=(txt.size[0]/20, txt.size[1]*18/20), text=author, font=font_attrib, fill=(255-xclr1,255-xclr2,255-xclr3,opac))
+    draw.text(xy=(text_wrapped_image.size[0]/20, text_wrapped_image.size[1]/20), text=wrapped_text, font=font, fill=(255-xclr1,255-xclr2,255-xclr3,opac))
+    draw.text(xy=(text_wrapped_image.size[0]/20, text_wrapped_image.size[1]*18/20), text=author, font=font_attrib, fill=(255-xclr1,255-xclr2,255-xclr3,opac))
 
+    return text_wrapped_image
+
+
+def generate_image_and_quote(num_words=3):
+    
+    # Request a quote from consol.csv according to the topic_to_obtain
+    quote, author = get_quote()
+
+    # Choose two (or num_words) random words from the quote
+    query = choose_random_word_from_quote(quote, num_words)
+
+    # Obtain an image on the basis of the query
+    my_quote_image, photo_link, photo_author, photographer_url = get_img(query)
+
+    # Generate an image of the same size, with transparent background, and text wrapped
+    text_wrapped_image = gen_text_wrapped_image(my_quote_image.size, quote, author)
+
+    # Specify a brightness modifier for the image
     applier = ImageEnhance.Brightness(my_quote_image.convert("RGBA"))
     
-    return Image.alpha_composite(applier.enhance(.7), txt), photo_link, photo_author, photographer_url
+    # Composite the final image from the quote image darkened slightly, and the text wrapped image
+    final_image = Image.alpha_composite(applier.enhance(.7), text_wrapped_image)
+    
+    return final_image, photo_link, photo_author, photographer_url
 
 
 
 def main():
     container = st.container()
-    image, photo_link, photographer, photographer_url = generate_image_and_quote(num_words=2)
+    image, photo_link, photographer, photographer_url = generate_image_and_quote()
     container.image(image)
-    st.markdown('---')
     if st.button('Generate another'): container.empty()
     st.markdown('---')
     st.markdown('''<small>Photos provided by [Pexels](https://www.pexels.com), quotations from various sources.</small>''', unsafe_allow_html = True)
     st.markdown('''<small>This [photo]({}) was taken by [{}]({}) on Pexels.</small>'''.format(photo_link, photographer, photographer_url), unsafe_allow_html = True)
-    
+    st.markdown('---')
+
     return None
 
 
